b8fba81296dfad9275b864623ab47406067eda52
Shantanu Kumar <shan.raj93@gmail.com>
2025-05-02 23:06:21 +0530

    (technical): init commit

---
+++ src/test/java/com/shann/splitwise/SplitwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shann.splitwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
